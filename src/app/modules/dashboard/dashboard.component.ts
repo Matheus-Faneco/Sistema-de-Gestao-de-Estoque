f13a4a9ff28f5d803b8f03554a3c4600b01d0255
Matheus-Faneco <matheusfaneco2009@hotmail.com>
2024-08-29 19:50:31 -0300

    adicao de delete e dashboard funcional

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,43 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartData, ChartOptions } from 'chart.js';
-import {Marca} from "../../models/marca";
-
+import { Entrada } from "../../models/entrada";
+import { Venda } from '../../models/venda';
+import { ApiService } from '../../service/api.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css'] // Corrigido de 'styleUrl' para 'styleUrls'
 })
 export class DashboardComponent implements OnInit {
-  data: any;
+  dataEntrada: Entrada[] = [];
+  dataVenda: Venda[] = [];
+  data: ChartData<'line'>;
+  options: ChartOptions<'line'>;
 
-  options: any;
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
+    this.getEntrada();
+    this.getVendas()
+
     const documentStyle = getComputedStyle(document.documentElement);
     const textColor = documentStyle.getPropertyValue('--text-color');
     const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
     const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
-
-    this.data = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'First Dataset',
-          data: [65, 59, 80, 81, 56, 55, 40],
-          fill: false,
-          borderColor: documentStyle.getPropertyValue('--blue-500'),
-          tension: 0.4
-        },
-        {
-          label: 'Second Dataset',
-          data: [28, 48, 40, 19, 86, 27, 90],
-          fill: false,
-          borderColor: documentStyle.getPropertyValue('--pink-500'),
-          tension: 0.4
-        }
-      ]
-    };
 
     this.options = {
       maintainAspectRatio: false,
@@ -56,7 +43,6 @@ export class DashboardComponent implements OnInit {
           },
           grid: {
             color: surfaceBorder,
-            drawBorder: false
           }
         },
         y: {
@@ -65,11 +51,61 @@ export class DashboardComponent implements OnInit {
           },
           grid: {
             color: surfaceBorder,
-            drawBorder: false
           }
         }
       }
     };
   }
 
+
+  atualizarGrafico() {
+    const documentStyle = getComputedStyle(document.documentElement);
+    this.data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'Entrada Dataset',
+          data: this.dataEntrada.map(entry => entry.quantity),
+          fill: false,
+          borderColor: documentStyle.getPropertyValue('--blue-500'),
+          tension: 0.4
+        },
+        {
+          label: 'Second Dataset',
+          data: [1,2,3],
+          fill: false,
+          borderColor: documentStyle.getPropertyValue('--pink-500'),
+          tension: 0.4
+        }
+      ]
+    };
+  }
+
+  getEntrada() {
+    this.api.getEntrada().subscribe(
+      (response: Entrada[]) => {
+        this.dataEntrada = response;
+        this.atualizarGrafico();
+        console.log('Array entradas:', this.dataEntrada);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getVendas() {
+    this.api.getVenda().subscribe(
+      (response: Venda[]) => {
+        this.dataVenda = response;
+        this.atualizarGrafico();
+        console.log('Array vendas:', this.dataVenda);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
+
+
